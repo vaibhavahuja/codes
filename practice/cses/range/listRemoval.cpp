e9aa21fda1964735475e028095e5d31c4dc8e060
vaibhavahuja <vaibhavahuja@outlook.com>
2020-06-22 19:41:27 +0530

    solved list removal

--- practice/cses/range/listRemoval.cpp
+++ practice/cses/range/listRemoval.cpp
@@ -1,36 +1,93 @@
-// 2 6 1 4 2
-// 1 2 3 4 5
-//     1 1 1
-// 1 1 2 2 3
-// 1 2 3 3 4
-
-// //
-// get(3)-> 1 
-// 3-> end (add +1);
-// get(1)-> 2
-// 1->end (add+1)cp
-// get(3)->goTo(3)->get(3+2)->get(5)->2
-// 5->end (add+1)
-// get(1)->goTo(1)->get(1+1)->get(2)->6
-// 2->end (add+1)
-// get(3)-> goTo(3) -> get(3+3)->get(6) : does not exist
-//solution fails
-//will think more about this problem tonight
-
-//to do later : https://cses.fi/problemset/task/1749
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long int
-#define vi vector<int>
-#define pii pair<int, int>
+#define vi vector<ll>
+#define pii pair<ll, ll>
 #define vp vector<pii>
 #define vs vector<string>
-#define mii map<int, int>
-void show(auto a){for(int i=0;i<a.size();i++){cout<<a[i]<<" ";}cout<<endl;}
+#define mii map<ll, ll>
+void show(auto a){for(ll i=0;i<5;i++){cout<<a[i]<<" ";}cout<<endl;}
+
+vi t(8e5+5), a(2e5+5);
+
+//building range sum query
+void build(ll v, ll tl, ll tr){
+	if(tl == tr){
+		t[v] = a[tl];
+		return;
+	}
+	ll tm = (tl+tr)/2;
+	build(v*2, tl, tm);
+	build(v*2+1, tm+1, tr);
+	t[v] = t[v*2]+t[v*2+1];
+}
+	
+//returns sum from 1 to j
+ll getSum(ll v, ll tl, ll tr, ll l, ll r){
+	if(l > r) return 0;
+	if(l == tl && r == tr){
+		return t[v];
+	}
+	ll tm = (tl + tr)/2;
+	return getSum(v*2, tl, tm, l, min(r, tm)) + getSum(v*2+1, tm+1, tr, max(l, tm+1), r);
+}
+
+void update(ll v, ll tl, ll tr, ll pos){
+	if(tl==tr){
+		t[v] -= 1;
+		return;
+	}
+
+	ll tm = (tl+tr)/2;
+	if(pos <= tm){
+		update(v*2, tl, tm, pos);
+	}else{
+		update(v*2+1, tm+1, tr, pos);
+	}
+	t[v] = t[v*2]+t[v*2+1];
+}
 
 int main(){
   ios_base::sync_with_stdio(false);
 
-  cout<<"heyy"<<endl;
+  ll n;
+  cin>>n;
+  vi b(n);
+  for(ll i = 0; i < n; i++){
+  	cin>>b[i];
+  	a[i] = 1;
+  }
+  build(1, 0, n-1);
+
+  for(ll i = 0; i < n; i++){
+  	ll p;
+  	cin>>p;
+  	// cout<<p<<endl;
+  	ll x = p;
+  	ll l = 0, h = n-1;
+  	while(l<h){
+  		ll mid = (l+h)/2;
+  		ll sum = getSum(1, 0, n-1, 0, mid);
+  		if(sum < x){
+  			l = mid+1;
+  		}else if(sum > x){
+  			h = mid-1;
+  		}else{
+  			if(a[mid] == 1){
+  				// cout<<"removing index "<<mid<<" and sum is "<<sum<<" and value is "<<x<<endl;
+  				l = mid;
+  				break;
+  			}else{
+  				h = mid-1;
+  			}
+  		}
+  	}
+  	a[l] = 0;
+  	update(1, 0, n-1, l);
+  	cout<<b[l]<<" ";
+
+  }
+  cout<<endl;
+  
 
 }
